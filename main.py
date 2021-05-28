68c9e8cc73a4d0f438c2d4d3830379e634f48ca7
Jererepe <84934066+Jererepe@users.noreply.github.com>
2021-05-28 15:34:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import os
 
 bot: Bot = commands.Bot(command_prefix='')
 
 
-@bot.command(aliases=['абоба'])
+@bot.command(aliases=('абоба',))
 async def cmd0(ctx):
-    await ctx.send('напиши "команды" и увидишь список команд')
+    await ctx.send('напиши "cmds" и увидишь список команд')
 
 
 @bot.command(aliases=['хуй'])
@@ -21,9 +21,11 @@ async def funcmd2(ctx):
 
 
 @bot.command(aliases=['команды'])
-async def cmd2(ctx):
-    await ctx.send('я могу принести тебе мячик! просто попроси!\nкоманда - мяч!\nclear *число* - очистит чат (писать '
-                   'кол-во сообщений(до 100), учитывая эту команду!)')
+async def cmds(ctx, amount=1):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("я могу принести тебе мячик! просто попроси!\nкоманда - мяч!\n\n=====доступна только сенату и выше "
+                   "стоящим!=====\nclear *число* - очистит чат (писать "
+                   "кол-во сообщений(до 100), учитывая эту команду!)\n=====доступна только сенату и выше стоящим!=====")
 
 
 @bot.command(aliases=['мяч!'])
@@ -32,15 +34,26 @@ async def cmd3(ctx):
 
 
 @bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
 async def clear(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
 
-@bot.command(pass_context=True)
-async def привет(ctx, amount=1):
+@bot.command(aliases=['негры'])
+async def Niggers(ctx, amount=1):
     await ctx.channel.purge(limit=amount)
-    author = ctx.message.author
-    await ctx.send(f'привет {author.mention}')
+    await ctx.send('''I HATE NIGGERS''')
+
+
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def mute(ctx, member: discord.Member):
+    await ctx.channel.purge(limit=1)
+
+    mute_role = discord.utils.get(ctx.message.guild.roles, name='MUTE')
+
+    await member.add_roles(mute_role)
+    await ctx.send(f'У {member.mention}, ограничение чата, потому что он вел себя как чмо')
 
 
 @bot.command(aliases=['Эпики'])
@@ -65,6 +78,6 @@ async def on_member_join(ctx, member):
     """это же Азамат Айталиев!."""
     await ctx.send(f'Добро пожаловать {member}! Напиши "команды" чтобы узнать мои команды.')
 
-token = os.environ.get('BOT_TOKEN')
 
-bot.run (str(token))
+token = os.environ.get('BOT_TOKEN')
+Bot.run(str(token))
